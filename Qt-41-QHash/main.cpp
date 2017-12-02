30b9d47e00fd3aa4ce7d8b2f2478efec9e97b449
doozy <slobodan_kletnikov@hotmail.com>
2017-12-02 11:22:59 +0100

    p41

---
+++ Qt-41-QHash/main.cpp
@@ -0,0 +1,27 @@
+#include <QCoreApplication>
+#include <QHash>
+#include <QDebug>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    QHash<QString, QString> map;
+
+    map.insert("Doozy", "Hello");
+    map.insert("Hello", "World");
+
+    QHashIterator<QString, QString> mInt(map);
+
+    while (mInt.hasNext()) {
+        mInt.next();
+        qDebug() << mInt.key() << " - " << mInt.value();
+    }
+
+    for(QString string : map.keys()) {
+        //qDebug() << map[string];
+        qDebug() << map.value(string);
+    }
+
+    return a.exec();
+}

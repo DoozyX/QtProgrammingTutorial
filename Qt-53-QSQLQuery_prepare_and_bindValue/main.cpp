b97d7de405b579588587fbb5101360f1ee74c09b
doozy <slobodan_kletnikov@hotmail.com>
2017-12-02 21:12:00 +0100

    p53

---
+++ Qt-53-QSQLQuery_prepare_and_bindValue/main.cpp
@@ -0,0 +1,37 @@
+#include <QCoreApplication>
+#include <QtSql>
+#include <QDebug>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
+
+    db.setConnectOptions();
+    db.setDatabaseName("DRIVER={SQL SERVER};SERVER=DOOZY;DATABASE=QtTutorial;Trusted_Connection=Yes");
+
+    if(db.open()) {
+        qDebug() << "Connect - OK";
+
+        QString command = "INSERT INTO People (Name, Surname) VALUES (:name, :surname)";
+        QSqlQuery query;
+
+        query.prepare(command);
+        query.bindValue(":name", "Doozy4");
+        query.bindValue(":surname", "Dzx4");
+
+        if (query.exec()) {
+            qDebug() << "Query - OK";
+        } else {
+            qDebug() << "Query - ERROR   = " << db.lastError();
+        }
+
+        qDebug() << "Connect - Closing...";
+        db.close();
+    } else {
+        qDebug() << "Connect - ERROR = " << db.lastError();
+    }
+
+    return a.exec();
+}
